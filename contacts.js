300f34920e8a973b657f84b5b45a82379c733208
okankpl <okan149@gmail.com>
2024-02-23 17:30:37 +0100

    adding style to contactList

--- contacts.js
+++ contacts.js
@@ -1,75 +1,100 @@
-const STORAGE_TOKEN = 'PMSYFRVR552SZW6MAG0T95301L1BCNVFHWSKVHMK';
-const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+const STORAGE_TOKEN = "PMSYFRVR552SZW6MAG0T95301L1BCNVFHWSKVHMK";
+const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 let contacts = [];
 
 async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN };
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
-        .then(res => res.json());
+  const payload = { key, value, token: STORAGE_TOKEN };
+  return fetch(STORAGE_URL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+  }).then((res) => res.json());
 }
 
 async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => {
-        // Verbesserter code
-        if (res.data) { 
-            return res.data.value;
-        } throw `Could not find data with key "${key}".`;
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      // Verbesserter code
+      if (res.data) {
+        return res.data.value;
+      }
+      throw `Could not find data with key "${key}".`;
     });
 }
-async function init(){
-    loadContacts();
-    renderContacts();
+async function init() {
+  loadContacts();
+  renderContacts();
 }
-async function loadContacts(){
-    try {
-        const result = await getItem('contacts');
-        contacts = JSON.parse(result);
-    } catch(e){
-        console.error('Loading error:', e);
-        contacts = [];
-    }
-    renderContacts();
+async function loadContacts() {
+  try {
+    const result = await getItem("contacts");
+    contacts = JSON.parse(result);
+  } catch (e) {
+    console.error("Loading error:", e);
+    contacts = [];
+  }
+  renderContacts();
 }
 
 async function save() {
-    saveBtn.disabled = true;
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let telefon = document.getElementById('telefon').value;
+  saveBtn.disabled = true;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let telefon = document.getElementById("telefon").value;
 
-    contacts.push({
-        name: name,
-        email: email,
-        telefon: telefon,
-    });
-    await setItem('contacts', JSON.stringify(contacts));
+  contacts.push({
+    name: name,
+    email: email,
+    telefon: telefon,
+  });
+  await setItem("contacts", JSON.stringify(contacts));
 }
 
 function clearForm() {
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('telefon').value = '';
-    saveBtn.disabled = false;
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("telefon").value = "";
+  saveBtn.disabled = false;
 }
 
-
 function renderContacts() {
-    const sortedContacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
-    const contactsContainer = document.getElementById('contactsList');
-    
-    let content = '';
-    let currentLetter = '';
+  const sortedContacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
+  const contactsContainer = document.getElementById("contactsList");
 
-    for (let i = 0; i < sortedContacts.length; i++) {
-        const contact = sortedContacts[i];
-        const firstLetter = contact.name[0].toUpperCase();
-        
-        if (currentLetter !== firstLetter) {
-            currentLetter = firstLetter;
-            content += `<h2>${currentLetter}</h2>`;
-        }
-        content += `<div>${contact.name} - ${contact.email} - ${contact.telefon}</div>`;
+  let content = "";
+  let currentLetter = "";
+
+  for (let i = 0; i < sortedContacts.length; i++) {
+    const contact = sortedContacts[i];
+    const firstLetter = contact.name[0].toUpperCase();
+    const parts = contact.name.split(' ');
+    const lastName = parts[parts.length - 1];
+    const secondLetter = lastName[0].toUpperCase();
+
+    if (currentLetter !== firstLetter) {
+      currentLetter = firstLetter;
+      content += `
+                <div>
+                    <h4>${currentLetter}</h4>
+                    <div class="underline"></div>
+                </div>`;
     }
-    contactsContainer.innerHTML = content;
+    content += generateContacts(contact.email, contact.name, secondLetter,firstLetter);
+  }
+  contactsContainer.innerHTML = content;
+}
+
+function generateContacts(email, name, secondLetter,firstLetter) {
+  return /*HTML*/ `
+    <div class="contact">
+        <div class="contact-header">
+            <span>${firstLetter}${secondLetter}</span>
+        </div> 
+        <div class="contact-info">
+            <div>${name}</div>
+            <div>${email}</div>
+        </div>
+    </div>
+    `;
 }
