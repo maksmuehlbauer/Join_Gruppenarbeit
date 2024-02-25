34127a0cb31cca3cd34b55bb71213dafe10af982
Bort190 <heiko.nevoigt@googlemail.com>
2024-02-25 15:40:19 +0100

    addTask assign to functionality

--- addtask.js
+++ addtask.js
@@ -32,30 +32,77 @@ let userDataBase = [
                 "email": "m.malte@web.de",
                 "phone": "+49 172 334 556 87"
             },
+            {
+                "name": "Eva",
+                "email": "m.malte@web.de",
+                "phone": "+49 172 334 556 87"
+            },
         ],
     }
 ]
-
+let assignedContacts = [];
 let prioButtonsColor = document.querySelectorAll('.prio-container button');
+let assignedToMenuOpen = false;
 
-function setPriority(priority){
+function setPriority(priority) {
     for (let i = 0; i < 3; i++) {
         prioButtonsColor[i].style.backgroundColor = 'white';
-        
     }
-    if(priority == 'high'){
+    if (priority == 'high') {
         userDataBase[0].tasks.prio = 2;
         document.getElementById('highPrioButton').style.backgroundColor = 'rgba(255, 61, 0, 1)';
     }
-    else if(priority == 'medium'){
+    else if (priority == 'medium') {
         userDataBase[0].tasks.prio = 2;
         document.getElementById('mediumPrioButton').style.backgroundColor = 'rgba(255, 168, 0, 1)';
     }
-    else if(priority == 'low'){
+    else if (priority == 'low') {
         userDataBase[0].tasks.prio = 2;
         document.getElementById('lowPrioButton').style.backgroundColor = 'rgba(122, 226, 41, 1)';
     }
-
-    
 }
 
+
+function openAssignContainer() {
+    if (assignedToMenuOpen == false) {
+        document.getElementById('contacts-to-assign-container').classList.remove('d-none');
+        assignedToMenuOpen = true;
+    }
+    else {
+        document.getElementById('contacts-to-assign-container').classList.add('d-none');
+        assignedToMenuOpen = false;
+    }
+}
+
+function renderAddTaskPage() {
+    let assignedToList = document.getElementById('contacts-to-assign-list');
+    for (let i = 0; i < userDataBase[0].contacts.length; i++) {
+        assignedToList.innerHTML += `<li class="" id="contact${i}" onclick="assignContact('${userDataBase[0].contacts[i].name}', 'contact${i}')">${userDataBase[0].contacts[i].name}</li>`
+    };
+}
+
+function assignContact(contactName, contactID) {
+
+    let index = assignedContacts.indexOf(contactName);
+    if (index === -1) {
+        assignedContacts.push(contactName);
+        document.getElementById(contactID).classList.add('checked');
+    }
+    else {
+        assignedContacts.splice(index, 1);
+        document.getElementById(contactID).classList.remove('checked');
+    }
+    let buttonText = document.getElementById('buttontext');
+    if (assignedContacts.length == 0) {
+        buttonText.innerHTML = `Select contacts to assign`;
+    }
+    else {
+        buttonText.innerHTML = ``;
+    }
+
+    for (let i = 0; i < assignedContacts.length; i++) {
+        buttonText.innerHTML += `<div class="assigned-contacts-button">${assignedContacts[i]}</div>`;
+    }
+
+
+}
