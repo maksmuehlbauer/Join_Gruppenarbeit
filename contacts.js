1e382dd2dd9a0a24a003d68a82a862cca7bd0656
okankpl <okan149@gmail.com>
2024-03-03 14:41:41 +0100

    finished addContactCard

--- contacts.js
+++ contacts.js
@@ -4,6 +4,7 @@ let contacts = [];
 let contactStatus = false;
 let showEditOptionsStatus;
 let contactOpenedStatus = false;
+let contactIndex;
 
 async function setItem(key, value) {
   const payload = { key, value, token: STORAGE_TOKEN };
@@ -18,7 +19,6 @@ async function getItem(key) {
   return fetch(url)
     .then((res) => res.json())
     .then((res) => {
-      // Verbesserter code
       if (res.data) {
         return res.data.value;
       }
@@ -48,9 +48,13 @@ async function createContact() {
     phone: phone,
   });
   await setItem("contacts", JSON.stringify(contacts));
+  renderContacts();
+  closeAddContactCard();
+  clearForm();
 }
 
 function clearForm() {
+  let saveBtn = document.getElementById('saveBtn');
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("phone").value = "";
@@ -60,44 +64,21 @@ function clearForm() {
 function renderContacts() {
   const sortedContacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
   const contactsContainer = document.getElementById("contactsList");
-
   let content = "";
   let currentLetter = "";
-
   for (let i = 0; i < sortedContacts.length; i++) {
     const contact = sortedContacts[i];
     const firstLetter = contact.name[0].toUpperCase();
     const parts = contact.name.split(" ");
     const lastName = parts[parts.length - 1];
     const secondLetter = lastName[0].toUpperCase();
-
     if (currentLetter !== firstLetter) {
       currentLetter = firstLetter;
-      content += `
-                <div>
-                    <h4 class="contact-headline-letter">${currentLetter}</h4>
-                    <div class="underline"></div>
-                </div>`;
+      content += generateHeadline(currentLetter);
     }
     content += generateContacts(contact.email,contact.name,secondLetter,firstLetter,i);
-    getRandomColor();
   }
   contactsContainer.innerHTML = content;
-}
-
-function generateContacts(email, name, secondLetter, firstLetter, i) {
-  const contactHeaderColor = getRandomColor();
-  return /*HTML*/ `
-      <div class="contact" id="${i}" onclick="openContact(${i})">
-          <div class="contact-header" style="background-color: ${contactHeaderColor};">
-              <span>${firstLetter}${secondLetter}</span>
-          </div> 
-          <div class="contact-info">
-              <div class="name">${name}</div>
-              <div class="email-contact">${email}</div>
-          </div>
-      </div>
-      `;
 }
 
 function getRandomColor() {
@@ -121,19 +102,8 @@ function openContact(i) {
   contactStatus = true;
 }
 
-function closeContact() {
-  if (contactStatus) {
-    contactsList.style.display = "block";
-    addContactImg.style.display = "flex";
-    contact.style.display = "none";
-    document.getElementById("center-contacts").classList.add("center");
-    contact.style.backgroundColor = "#FFFFFF";
-  }
-  contactStatus = false;
-  contactOpenedStatus = false;
-}
-
 function showContact(i) {
+  contactIndex = i;
   contactOpenedStatus = true;
   let content = document.getElementById("contact");
   content.innerHTML = "";
@@ -146,12 +116,100 @@ function showContact(i) {
   const name = contact.name;
   const phone = contact.telefon;
   const email = contact.email;
-  content.innerHTML += generateContact(firstLetter,secondLetter,name,phone,email,contactHeaderColor);
+  content.innerHTML += generateContact(firstLetter,secondLetter,name,phone,email,contactHeaderColor,i);
 }
 
-function generateContact(firstLetter,secondLetter,name,telefon,email,contactHeaderColor) {
+function closeContact() {
+  if (contactStatus) {
+    contactsList.style.display = "block";
+    addContactImg.style.display = "flex";
+    contact.style.display = "none";
+    document.getElementById("center-contacts").classList.add("center");
+    contact.style.backgroundColor = "#FFFFFF";
+  }
+  contactStatus = false;
+  contactOpenedStatus = false;
+}
+
+function showEditOptions() {
+  let editOptions = document.getElementById("editContactOptions");
+  editOptions.classList.add("edit-contacts-options-active");
+  editContactImg.style.display = "none";
+  showEditOptionsStatus = false;
+}
+
+function openEditContact() {
+
+}
+
+async function deleteContact() {
+    contacts.splice(contactIndex, 1)
+    await setItem("contacts", JSON.stringify(contacts)); 
+    renderContacts();
+    closeContact();
+}
+
+function openAddContact() {
+  document.getElementById("center-add-card").classList.add("active");
+  document.getElementById("addContactCard").classList.add("active");
+  addContactImg.style.display = 'none';
+  overlayContacts.style.display = 'block';
+}
+
+function closeAddContactCard() {
+  const addContactCard = document.getElementById("addContactCard");
+  const centerAddCard = document.getElementById("center-add-card");
+  addContactCard.classList.remove("active");
+  setTimeout(() => {
+    centerAddCard.classList.remove("active");
+    addContactImg.style.display = 'flex';
+    overlayContacts.style.display = 'none';
+  }, 500); 
+}
+
+document.addEventListener("click", function (event) {
+  if (contactOpenedStatus) {
+    let isClickInsideOptions = document.getElementById("editContactOptions").contains(event.target);
+    let isClickInsideImg = document.getElementById("editContactImg").contains(event.target);
+    if ((editContactOptions.style.display = "flex" && !isClickInsideImg)) {
+      if (!isClickInsideOptions && !showEditOptionsStatus) {
+        document.getElementById("editContactOptions").classList.remove("edit-contacts-options-active");
+        editContactImg.style.display = "flex";
+        showEditOptionsStatus = true;
+      }
+    }
+  }
+});
+
+/* Templates*/
+
+function generateHeadline(currentLetter) {
   return /*HTML*/ `
-  <div class="contact-container">
+    <div>
+                    <h4 class="contact-headline-letter">${currentLetter}</h4>
+                    <div class="underline"></div>
+                </div>
+  `;
+}
+
+function generateContacts(email, name, secondLetter, firstLetter, i) {
+  const contactHeaderColor = getRandomColor();
+  return /*HTML*/ `
+      <div class="contact" id="${i}" onclick="openContact(${i})">
+          <div class="contact-header" style="background-color: ${contactHeaderColor};">
+              <span>${firstLetter}${secondLetter}</span>
+          </div> 
+          <div class="contact-info">
+              <div class="name">${name}</div>
+              <div class="email-contact">${email}</div>
+          </div>
+      </div>
+      `;
+}
+
+function generateContact(firstLetter,secondLetter,name,telefon,email,contactHeaderColor,i) {
+  return /*HTML*/ `
+  <div class="contact-container" id=${i}>
     <div class="back-to-contacts-img">
     <img onclick="closeContact()" src="assets/img/back.png" alt="">
     </div>
@@ -170,7 +228,6 @@ function generateContact(firstLetter,secondLetter,name,telefon,email,contactHead
       <div class="email-contact">${email}</div>
       <p class="email-contact-opened">Phone</p>
       <div class="phone-contact">${telefon}</div>
-      
   </div>
    <div id="editContactImg" class="editContactImg" onclick="showEditOptions()">
    <img src="/assets/img/more_vert.png" alt="">
@@ -181,54 +238,4 @@ function generateContact(firstLetter,secondLetter,name,telefon,email,contactHead
    </div>
 </div>
     `;
-}
-
-function showEditOptions() {
-  let editOptions = document.getElementById("editContactOptions");
-  editOptions.classList.add("edit-contacts-options-active");
-  editContactImg.style.display = "none";
-  showEditOptionsStatus = false;
-}
-
-document.addEventListener("click", function (event) {
-  if (contactOpenedStatus) {
-    let isClickInsideOptions = document
-      .getElementById("editContactOptions")
-      .contains(event.target);
-    let isClickInsideImg = document
-      .getElementById("editContactImg")
-      .contains(event.target);
-    if ((editContactOptions.style.display = "flex" && !isClickInsideImg)) {
-      if (!isClickInsideOptions && !showEditOptionsStatus) {
-        // zum schließen der Optionen
-        document
-          .getElementById("editContactOptions")
-          .classList.remove("edit-contacts-options-active");
-        editContactImg.style.display = "flex";
-        showEditOptionsStatus = true;
-      }
-    }
-  }
-});
-
-function openEditContact() {}
-
-function deleteContact() {}
-
-function openAddContact() {
-  document.getElementById("center-add-card").classList.add("active");
-  document.getElementById("addContactCard").classList.add("active");
-  addContactImg.style.display = 'none';
-  overlayContacts.style.display = 'block';
-}
-
-function closeAddContactCard() {
-  const addContactCard = document.getElementById("addContactCard");
-  const centerAddCard = document.getElementById("center-add-card");
-  addContactCard.classList.remove("active");
-  setTimeout(() => {
-    centerAddCard.classList.remove("active");
-    addContactImg.style.display = 'flex';
-    overlayContacts.style.display = 'none';
-  }, 500); 
 }
