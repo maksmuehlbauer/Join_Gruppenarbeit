b5bc54e77198cbdcbafbef23aedf8f4b40ffe16a
maksmuehlbauer <nakeda1987@gmail.com>
2024-03-19 22:27:39 +0100

    new JS file added

--- script.js
+++ script.js
@@ -176,20 +176,6 @@ function showMenu() {
     }
 }
 
-/**
- * function, reacts on clickevent. Closes Menu (top-right) if clicking outside the open menu container
- * @param {MouseEvent} event - click event
- */
-document.addEventListener("click", function(event) {
-    let bodyBackground = event.target;
-    let menuContainer = document.getElementById("user-menu");
-    let userContainer = document.getElementById("user-circle");
-    if (!menuContainer.contains(bodyBackground) && !userContainer.contains(bodyBackground)) {
-        if (!menuEnabled)
-            showMenu();
-    }
-});
-
 
 /**
  * Toggles the visibility of the hidden box element.
